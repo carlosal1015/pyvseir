4f4942d8e861ad64ddd4d08e70df65f8b361b7e4
Carlos Aznarán Laos <caznaranl@uni.pe>
2021-10-17 09:28:29 -0500

    Update method.py

--- src/pyvseir/method.py
+++ src/pyvseir/method.py
@@ -2,28 +2,27 @@
 # -*- coding: utf-8 -*-
 
 
-def ForwardEuler(fun, y0, a, b, n):
+import numpy as np
 
-    import numpy as np
 
+def ForwardEuler(fun: function, y0: np.array, a: float, b: float, n: int) -> np.array:
     """[summary]
 
     Args:
         fun (function): Describes the right-hand side of the ODE.
-        y0 (array): Initial value.
+        y0 (np.array): Initial value.
         a (float): Left boundary of the interval [a,b].
         b (float): Right boundary of the interval [a,b].
-        n (integer): Number of discretization points for the interval is n + 1.
+        n (int): Number of discretization points for the interval is n + 1.
 
     Returns:
-        y (array): Contains the solution at the k-th time step as k-th column.
+        np.array: Contains the solution at the k-th time step as k-th column.
     """
-
     # set step size
-    h = (b - a) / n
+    h: float = (b - a) / n
 
     # initialize solution
-    y = np.zeros((np.size(y0), n + 1))
+    y: np.zeros = np.zeros((np.size(y0), n + 1))
 
     # set initial value
     y[:, 0] = y0
